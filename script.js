591b801691c657c07f21519920a91eb79a162195
miraimo <lellymohammed2006@gmail.com>
2026-04-11 11:36:42 +0100

    update script.js file

--- script.js
+++ script.js
@@ -1,62 +1,101 @@
 let Playerchoice;
 
-let rounds = 0;
+let rounds = 1 ;
+
+
 let computer = 0;
 let player = 0;
 
-function interactive_Prisoners(){
-    let Bootchoice = Math.floor(Math.random() * 2);
+let chacker = false;
 
-    if(Bootchoice === Playerchoice){
+function random_choise_and_if(){
+    const bool = [false, true]
+    var Bootchoice = bool[Math.floor(Math.random() * 2)];
+    if(Bootchoice && Playerchoice){
+        rounds++;
+        computer += 3;
+        player += 3;
+        chacker = false;
+
         document.getElementById(`get-3`).innerHTML = "3";
         document.getElementById(`get-3p`).innerHTML = "3";
 
         document.getElementById(`result-5`).innerHTML = "1";
         document.getElementById(`result-5p`).innerHTML = "1";
 
-        rounds++;
-        computer += 3;
-        player += 3;
 
         document.getElementById('Rounsds').textContent = rounds;
         document.getElementById('computer-result').textContent = computer;
         document.getElementById('player-result').textContent = player;
 
-    } else if (Bootchoice === 1 && Playerchoice === 2)  {
+    } else if(!Bootchoice && !Playerchoice){
+        rounds++;
+        computer += 5;
+        player += 5;
+        chacker = false;
+
+        document.getElementById(`get-3`).innerHTML = "5";
+        document.getElementById(`get-3p`).innerHTML = "5";
+
+        document.getElementById(`result-5`).innerHTML = "1";
+        document.getElementById(`result-5p`).innerHTML = "1";
+
+
+        document.getElementById('Rounsds').textContent = rounds;
+        document.getElementById('computer-result').textContent = computer;
+        document.getElementById('player-result').textContent = player;
+    }else if (Bootchoice && !Playerchoice)  {
+        rounds++;
+        player += 5;
+        chacker = false;
+
+
         document.getElementById(`get-5`).innerHTML = "5";
         document.getElementById(`get-5p`).innerHTML = "0";
 
         document.getElementById(`result-5`).innerHTML = "5";
         document.getElementById(`result-5p`).innerHTML = "0";
 
-        rounds++;
-        player += 5;
-
         document.getElementById('Rounsds').textContent = rounds;
         document.getElementById('computer-result').textContent = computer;
         document.getElementById('player-result').textContent = player;
 
-    } else if (Bootchoice === 2 && Playerchoice === 1)  {
+    } else if (!Bootchoice && Playerchoice)  {
+        rounds++;
+        computer += 5;
+        chacker = true;
+
         document.getElementById(`get-5`).innerHTML = "0";
         document.getElementById(`get-5p`).innerHTML = "5";
 
         document.getElementById(`result-5`).innerHTML = "0";
         document.getElementById(`result-5p`).innerHTML = "5";
 
-        rounds++;
-        computer += 5;
 
         document.getElementById('Rounsds').textContent = rounds;
         document.getElementById('computer-result').textContent = computer;
         document.getElementById('player-result').textContent = player;
     }
+
+}
+
+
+function main(){
+    if(rounds !== 10){
+        if(!chacker){
+            random_choise_and_if()
+        }else{
+            Bootchoice = true;
+            random_choise_and_if()
+        }
+    }
 }
 
 function Cooperates(){
-    Playerchoice = 1;
-    interactive_Prisoners()
+    Playerchoice = false;
+    main()
 }
 function Betrays(){
-    Playerchoice = 2;
-    interactive_Prisoners()
+    Playerchoice = true;
+    main()
 }
